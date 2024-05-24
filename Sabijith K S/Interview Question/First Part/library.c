c5a73ff696d0a923aa4cac4a6637d735b3a15414
sabijithks-19 <sabijith@kefassoftwares.com>
2024-05-24 15:39:57 +0530

    Update library.c
    Added A Borrow Function and function to print list of last n borrowers for a book

--- Sabijith K S/Interview Question/First Part/library.c
+++ Sabijith K S/Interview Question/First Part/library.c
@@ -4,7 +4,8 @@
 #include "library.h"
 #include "quicksortiterative.h"
 #include "binarysearch.h"
-
+#include "addborrow.h"
+#include "printborrower.h"
 
 int main()
     
@@ -12,37 +13,38 @@ int main()
 {
     
     struct Library Book[10];
+    struct Borrow Borrower[5];
 
     
     strcpy(Book[0].book_name, "The Great Gatsby");
     strcpy(Book[0].book_author, "Scott Fitzgerald");
     Book[0].book_holder = -1;
-    Book[0].book_borrow_date = 0;
-    Book[0].book_return_date = 0;
+    Book[0].num_of_borrowers=0;
+    Book[0].borrower_list=NULL;
 
     strcpy(Book[1].book_name, "Alice In Wonderland");
     strcpy(Book[1].book_author, "Lewis Carrol");
     Book[1].book_holder = -1;
-    Book[1].book_borrow_date = 0;
-    Book[1].book_return_date = 0;
+    Book[1].num_of_borrowers=0;
+    Book[1].borrower_list=NULL;
 
     strcpy(Book[2].book_name, "Time Machine");
     strcpy(Book[2].book_author, "H G Wells");
-    Book[2].book_holder = 1;
-    Book[2].book_borrow_date = 12;
-    Book[2].book_return_date =20;
+    Book[2].book_holder = -1;
+    Book[2].num_of_borrowers=0;
+    Book[2].borrower_list=NULL;
 
     strcpy(Book[3].book_name, "Divine Comedy");
     strcpy(Book[3].book_author, "Dante");
     Book[3].book_holder = -1;
-    Book[3].book_borrow_date = 0;
-    Book[3].book_return_date = 0;
+    Book[3].num_of_borrowers=0;
+    Book[3].borrower_list=NULL;
     
     strcpy(Book[4].book_name, "Ancient Mariner");
     strcpy(Book[4].book_author, "Coleridge");
-    Book[4].book_holder = 1;
-    Book[4].book_borrow_date = 10;
-    Book[4].book_return_date =20;
+    Book[4].book_holder = -1;
+    Book[4].num_of_borrowers=0;
+    Book[4].borrower_list=NULL;
 
 
     quickSortIterative(Book, 0, 4);
@@ -55,7 +57,7 @@ int main()
     
     int option;
     
-    printf("\n\n1. Search For A Book\n2. Exit");
+    printf("\n\n1. Search For A Book\n2. Borrow A Book \n3. List The Borrowers Of Book \n4. Exit");
    
     while(1)
         
@@ -93,8 +95,7 @@ int main()
                   printf("Member");
               }
           
-              printf("\nBook Borrow Date : %d", Book[result].book_borrow_date);
-              printf("\nBook Return Date : %d", Book[result].book_return_date);
+              printf("\nNumber Of Borrowers : %d",Book[result].num_of_borrowers);
               printf("\n");
     
               if(Book[result].book_holder==-1)
@@ -119,6 +120,56 @@ int main()
 
          else if(option==2)
          {
+             int borrowerid=0;
+             printf("\nEnter The Name Of The Book To Borrow : ");
+             getchar(); 
+             fgets(search_name, sizeof(search_name), stdin);
+             search_name[strcspn(search_name, "\n")] = '\0';
+             int result = binarySearch(Book,0,4,search_name);
+             if(result==-1)
+             {
+                 printf("\nNo Such Book In Library.");
+             }
+             else
+             {
+                 printf("\nBorrowing A Book .....");
+                 printf("\n\nEnter Details Of Borrower.\n");
+                 printf("\nEnter Borrower Name : ");
+                 scanf("%s",Borrower[borrowerid].borrower_name);
+                 printf("Enter Borrow Date : ");
+                 scanf("%d",&Borrower[borrowerid].borrow_date);
+                 printf("Enter Return Date : ");
+                 scanf("%d",&Borrower[borrowerid].return_date);
+
+                 addBorrower(&Book[result], Borrower[borrowerid].borrower_name, Borrower[borrowerid].borrow_date, Borrower[borrowerid].return_date);
+                 Book[result].book_holder=1;
+                 
+             }
+             
+
+         }
+
+          else if(option==3)
+         {
+
+             printf("\nEnter The Book Name : ");
+             getchar(); 
+             fgets(search_name, sizeof(search_name), stdin);
+             search_name[strcspn(search_name, "\n")] = '\0';
+             int result = binarySearch(Book,0,4,search_name);
+             if(result==-1)
+             {
+                 printf("\nNo Such Book In Library.");
+             }
+             else
+             {
+                 printBorrowers(Book[result]);
+             }
+           
+         }
+
+         else if(option==4)
+         {
 
             printf("\nExiting .....");
             printf("\n\n");
@@ -130,3 +181,4 @@ int main()
     return 0;
 
 }
+
