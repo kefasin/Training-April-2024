3a36b7c7b111339c56303a426e91ac1865ccf345
sabijithks-19 <sabijith@kefassoftwares.com>
2024-05-24 15:43:32 +0530

    Update library.h
    changes to adapt with new functions and structure model

--- Sabijith K S/Interview Question/First Part/library.h
+++ Sabijith K S/Interview Question/First Part/library.h
@@ -1,13 +1,20 @@
 #define MAX_NAME_LENGTH 100
 
+struct Borrow
+{
+    char borrower_name[MAX_NAME_LENGTH];
+    int borrow_date;
+    int return_date;
+    struct Borrow *next_borrower;
+};
+
 struct Library
 {
     char book_name[MAX_NAME_LENGTH];
     char book_author[MAX_NAME_LENGTH];
     int  book_holder;
-    int book_borrow_date;
-    int book_return_date;
+    int num_of_borrowers;
+    struct Borrow *borrower_list;
     
 };
-
 
